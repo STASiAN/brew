93ba4284b223064dd45117ffb54ec763b0a9e1ff
Stanislav Shcherbakov <sshcherbakov@plesk.com>
2020-02-13 14:38:12 +0700

    update brew 15 sep 2021

--- brew.sh
+++ brew.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 if [ ! -f /usr/local/bin/brew ]; then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";fi
+
 brew tap homebrew/cask-fonts
-brew cask install aerial awscli docker osxfuse slack sourcetree telegram-desktop tor-browser transmission visual-studio-code vlc wireshark
-brew install asciinema bash-completion@2 bat binwalk cowsay direnv fping gawk git git-flow gnu-tar go hashcat httpie ipcalc iperf istioctl jq kubectx kubernetes-cli kubernetes-helm kubespy lolcat lsd mas midnight-commander mtr ncdu neofetch nmap p7zip pidof pv pwgen radare2 socat sshfs stern telnet tig tmux unrar watch wget wireguard-go wireguard-tools xhyve xz yara zsh-completion
-mas install 409183694 409201541 409203825 409789998 425264550 497799835 549083868 803453959 1037126344 1147396723 1153157709 1295203466
+
+echo BREW: iNSTALL F0RMULAe
+brew install asciinema awscli bat binwalk coreutils cowsay dep direnv dive findutils fping gawk git git-flow gnu-sed gnu-tar go hashcat httpie httpstat ipcalc iperf jq k9s krew kubectl kubectx kubespy lolcat lsd mas midnight-commander mtr ncdu neofetch nmap p7zip packer pidof pluto ponysay pv pwgen qrencode radare2 shellcheck sl snappy socat sshfs stern tektoncd-cli telnet tig tmux tree unrar watch wget wireguard-go wireguard-tools xz yara yq zsh-completion
+
+echo BREW: iNSTALL CASKs
+brew install docker font-hack-nerd-font osxfuse slack sourcetree telegram tor-browser transmission virtualbox visual-studio-code vlc wireshark xquartz
+
+echo MAS: iNSTALL APPs
+mas install 409183694 409201541 409203825 497799835 549083868 1147396723 1153157709 1295203466 1451685025 1482454543
