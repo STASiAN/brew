36debdafa6a87315911625e7464f28d9cb3e5500
STASiAN <mail@stasian.ru>
2021-09-15 17:47:50 +0700

    update hombrew bin path

--- brew.sh
+++ brew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f /usr/local/bin/brew ]; then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";fi
+if [ ! -f /opt/homebrew/bin/brew ]; then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";fi
 
 brew tap homebrew/cask-fonts
 
