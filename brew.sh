4089512fa435e0b55ce8826e4425a3a372185f55
STASiAN <mail@stasian.ru>
2021-09-15 17:50:10 +0700

    add brew shellenv remove unused formulae

--- brew.sh
+++ brew.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if [ ! -f /opt/homebrew/bin/brew ]; then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";fi
+if [ ! -f /opt/homebrew/bin/brew ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+fi
 
 brew tap homebrew/cask-fonts
 
 echo BREW: iNSTALL F0RMULAe
-brew install asciinema awscli bat binwalk coreutils cowsay dep direnv dive findutils fping gawk git git-flow gnu-sed gnu-tar go hashcat httpie httpstat ipcalc iperf jq k9s krew kubectl kubectx kubespy lolcat lsd mas midnight-commander mtr ncdu neofetch nmap p7zip packer pidof pluto ponysay pv pwgen qrencode radare2 shellcheck sl snappy socat sshfs stern tektoncd-cli telnet tig tmux tree unrar watch wget wireguard-go wireguard-tools xz yara yq zsh-completion
+brew install asciinema awscli bat binwalk coreutils cowsay dep direnv dive findutils fping gawk git git-flow gnu-sed gnu-tar go hashcat httpie httpstat ipcalc iperf jq k9s krew kubectl kubectx kubespy lolcat lsd mas midnight-commander mtr ncdu neofetch nmap p7zip packer pidof ponysay pv pwgen qrencode radare2 shellcheck sl socat sshfs stern tektoncd-cli telnet tig tmux tree watch wget wireguard-go wireguard-tools xz yara yq zsh-completion
 
 echo BREW: iNSTALL CASKs
 brew install --cask docker font-hack-nerd-font osxfuse slack sourcetree telegram tor-browser transmission virtualbox visual-studio-code vlc wireshark xquartz
